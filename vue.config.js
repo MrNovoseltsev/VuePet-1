c00f184afadb54398141461306d64f5e524cc573
MrNovoseltsev <135010699+MrNovoseltsev@users.noreply.github.com>
2023-07-04 10:58:24 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-  // publicPath: '/vue-app/',
+  publicPath: '/VuePet-1/',
   transpileDependencies: true,
   lintOnSave: false,
 });
